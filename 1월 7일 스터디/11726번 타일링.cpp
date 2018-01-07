99efcf98a611482b44697e9f0de0bcd3e1ecc7ab
HongSeongHwan <shhong407@naver.com>
2018-01-07 16:00:11 +0900

    Update 11726번 타일링.cpp

--- 1월 7일 스터디/11726번 타일링.cpp
+++ 1월 7일 스터디/11726번 타일링.cpp
@@ -1,19 +1,24 @@
+#include<stdio.h>
 #include<iostream>
 using namespace std;
-void main(){
-int c,p,p2,n;
-cin>>n;
-c=p=2;
-p2=1;
-for (int i=3;i<= n;i++){
-c=p+p2;
-c=c%10007;
-p2=p;
-p=c;
-}
-if (n == 1) {
-cout << "1";
-return;
-}
-cout<<c;
+int main() {
+	int c;
+	int p;
+	int p2;
+	int n;
+	cin >> n;
+	c = 2;
+	p = 2;
+	p2 = 1;
+	for (int i = 3; i <= n; i++) {
+		c = p + p2;
+		c = c % 10007;
+		p2 = p;
+		p = c;
+	}
+	if (n == 1) {
+		printf("1");
+		return 0;
+	}
+	printf("%d", c  );
 }
